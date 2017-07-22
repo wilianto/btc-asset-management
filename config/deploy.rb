95a5f210a4453a7d0733de8b395edba844edadaf
Wilianto Indrawan <wilianto@users.noreply.github.com>
2017-07-22 12:01:44 +0700

    add rails env to capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,7 @@ set :puma_workers,    0
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
+set :rails_env,       :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
